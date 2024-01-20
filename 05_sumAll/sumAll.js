a33c661641c782ca6fb5842c2ad5239f2987a277
KingFryRice <graysonbeatonrice@gmail.com>
2024-01-19 17:32:57 -0800

    Pass exercise five first four tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+  let num1;
+  let num2;
+
+  if (a > b) {
+    num2 = a;
+    num1 = b;
+  } else if (a < b) {
+    num1 = a;
+    num2 = b;
+  }
+
+  let sum = 0;
+
+  for (let i = num1; i <= num2; i++) {
+    if (num1 < 0 || num2 < 0) {
+      return "ERROR";
+    } else {  
+    sum += i;
+    }
+  }
+
+  return sum;
 
 };
 
