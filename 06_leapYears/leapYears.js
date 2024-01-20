3cf976e9fcc772f4207bff56cb21f980f089e1e6
KingFryRice <graysonbeatonrice@gmail.com>
2024-01-20 12:37:02 -0800

    Pass exercise six tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+  let isLeapYear = 0;
+
+  if (year % 4 === 0 &&
+    year % 100 !== 0 ||
+    year % 400 === 0) {
+    isLeapYear = true;
+  } else {
+    isLeapYear = false;
+  }
+  return isLeapYear;
 };
 
 // Do not edit below this line
