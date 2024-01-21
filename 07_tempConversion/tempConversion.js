92097270a31b4035d4d8da46ba44bf7b8b904f11
KingFryRice <graysonbeatonrice@gmail.com>
2024-01-20 16:50:19 -0800

    Pass exercise seven tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function roundDegrees(num, decimalPlaces) {
+  const factor = Math.pow(10, decimalPlaces);
+  return Math.round(num * factor) / factor;
+}
+
+const convertToCelsius = function(temp) {
+  let degrees = ((temp - 32) * (5/9));
+  return roundDegrees(degrees, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let degrees = (temp* (9/5) + 32);
+  return roundDegrees(degrees, 1);
 };
 
 // Do not edit below this line
